280f78f74754298b76a7932da4f1bb5fbf5cdf21
WangariNelly <nellywangari30@gmail.com>
2024-06-05 11:11:28 +0300

    Redux Installation and packages debug

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Container,AppBar,Typography,Grid,Grow } from '@material-ui/core';
 
+
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/memories.png';
+import memories from './images/memories.png'; 
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxwidth="lg">
-         <AppBar position="static" color="inherit">
-         <Typography variant='h2' align='center'> Memories</Typography>
-         <img src={memories} alt="memories" height="60"/>
+         <AppBar className={classes.appBar} position="static" color="inherit">
+         <Typography className={classes.heading} variant='h2' align='center'> Memories</Typography>
+         <img className={classes.img} src={memories} alt="memories" height="60"/>
          </AppBar>
          <Grow in>
             <Container>
